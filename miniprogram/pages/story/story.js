1987c810564c82892be4e19bdc37ae53884d718e
DESKTOP-D4QU1KE\YZ <bdq>
2021-06-28 15:05:44 +0800

    大改

--- miniprogram/pages/story/story.js
+++ miniprogram/pages/story/story.js
@@ -1,44 +1,131 @@
-// miniprogram/pages/story/story.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    mainActiveIndex: 0,
+    activeId: null,
+    items:[
+      {
+        // 导航名称
+        text: '童话故事',
+        // 禁用选项
+        // disabled: false,
+        // 该导航下所有的可选项
+        children: [
+          {
+            // 名称
+            text: '灰姑娘',
+            // id，作为匹配选中状态的标识
+            id: '灰姑娘',
+            // 禁用选项
+            // disabled: true,
+          },
+          {
+            text: '小红帽',
+            id: '小红帽',
+          },
+        ],
+      },
+      {
+        text: '睡前故事',
+        children: [
+          {
+            text: '见习死神',
+            id: '见习死神',
+          },
+          {
+            text: '我在等你',
+            id: '我在等你',
+          },
+          {
+            text: '月亮之子',
+            id: '月亮之子',
+          },
+          {
+            text: '零度的怀抱',
+            id: '零度的怀抱',
+          },
+          {
+            text: '尘世间',
+            id: '尘世间',
+          },
+          {
+            text: '酒',
+            id: '酒',
+          },
+          {
+            text: '想飞的猫',
+            id: '想飞的猫',
+          },
+          {
+            text: '神龙',
+            id: '神龙',
+          },
+          {
+            text: '遍插茱萸少一人',
+            id: '遍插茱萸少一人',
+          },
+          {
+            text: '最后一个太阳',
+            id: '最后一个太阳',
+          },
+          {
+            text: '就算你是妖又如何',
+            id: '就算你是妖又如何',
+          },
+          {
+            text: '英雄',
+            id: '英雄',
+          },
+          {
+            text: '桃妖(一)',
+            id: '桃妖(一)',
+          },
+          {
+            text: '桃妖(二)',
+            id: '桃妖(二)',
+          },
+        ],
+      },
+      {
+        text: '神话故事',
+        children: [
+          {
+            text: '后羿射日',
+            id: '后羿射日',
+          },
+          {
+            text: '夸父逐日',
+            id: '夸父逐日',
+          },
+        ],
+      },
+    ]
+  },
+  onClickNav({ detail = {} }) {
+    this.setData({
+      mainActiveIndex: detail.index || 0,
+    });
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
+  onClickItem({ detail = {} }) {
+    const activeId = this.data.activeId === detail.id ? null : detail.id;
+    this.setData({ activeId });
+    console.log(this.data.activeId)
+      wx.navigateTo({
+      url: `/pages/story/story_detail/story_detail?id=${this.data.activeId}`,
+    }) 
+  },
   onLoad: function (options) {
 
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
   onReady: function () {
 
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
   onShow: function () {
 
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
   onHide: function () {
 
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
   onUnload: function () {
 
   },
---
+++ miniprogram/pages/story/story_detail/story_detail.js
@@ -0,0 +1,79 @@
+// miniprogram/pages/story/story_detail/story_detail.js
+import jsonList from './../../../static.js'
+Page({
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    id: null,
+    story: {},
+    isPlaying: false // 是否播放
+  },
+  init(id) {
+    for (let val of jsonList) {
+      if (val.id == id) {
+        this.setData({
+          story: val
+        })
+      }
+    }
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    console.log(jsonList, 111)
+    this.init(options.id)
+    this.setData({ id: options.id })
+    wx.setNavigationBarTitle({
+      title: options.id
+    })
+  },
+  // 手动播放音频文件
+  audioClick() {
+    this.setData({
+      isPlaying: !this.data.isPlaying
+    })
+    if (this.data.isPlaying) {
+      this.AudioContext.play()
+    } else {
+      this.AudioContext.pause()
+    }
+  },
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    this.AudioContext = wx.createAudioContext('myAudio')
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {},
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {},
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {},
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {},
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {},
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {}
+})
